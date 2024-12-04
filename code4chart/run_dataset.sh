4be4e2e0fbaf81ff6b609db00bf291c9b1404138
YuweiYin <seckexyin@gmail.com>
2024-12-03 18:14:22 -0800

    C4C

--- code4chart/run_dataset.sh
+++ code4chart/run_dataset.sh
@@ -12,21 +12,29 @@ do
   idx=$(( $((idx)) + 1 ))
   # echo -e ">>> idx = ${idx}; val = ${val}"
   if [[ "${idx}" -eq 1 ]]; then
-    BSZ=${val}
+    TASK=${val}
   fi
   if [[ "${idx}" -eq 2 ]]; then
-    MAX_SEQ_LEN=${val}
+    BSZ=${val}
   fi
   if [[ "${idx}" -eq 3 ]]; then
-    TEXT_LLM=${val}
+    MAX_SEQ_LEN=${val}
   fi
   if [[ "${idx}" -eq 4 ]]; then
-    CODE_LLM=${val}
+    TEXT_LLM=${val}
   fi
   if [[ "${idx}" -eq 5 ]]; then
+    CODE_LLM=${val}
+  fi
+  if [[ "${idx}" -eq 6 ]]; then
     VLM=${val}
   fi
 done
+
+if [[ -z ${TASK} ]]
+then
+  TASK="1"
+fi
 
 if [[ -z ${BSZ} ]]
 then
@@ -53,28 +61,31 @@ then
   VLM="meta-llama/Llama-3.2-11B-Vision-Instruct"
 fi
 
-echo -e "TEXT_LLM: ${TEXT_LLM}"
-echo -e "CODE_LLM: ${CODE_LLM}"
-echo -e "VLM: ${VLM}"
-echo -e "BSZ: ${BSZ}"
-echo -e "MAX_SEQ_LEN: ${MAX_SEQ_LEN}"
-
 #CUDA="0,1"
 SEED=42
 ROOT_DIR="${HOME}/projects/def-carenini/yuweiyin"
 CACHE_DIR="${ROOT_DIR}/.cache/huggingface/"
 
-echo -e "\n >>> python3 run_dataset.py --task 1"
+echo -e "TASK: ${TASK}"
+echo -e "BSZ: ${BSZ}"
+echo -e "MAX_SEQ_LEN: ${MAX_SEQ_LEN}"
+echo -e "TEXT_LLM: ${TEXT_LLM}"
+echo -e "CODE_LLM: ${CODE_LLM}"
+echo -e "VLM: ${VLM}"
+echo -e "ROOT_DIR: ${ROOT_DIR}"
+echo -e "CACHE_DIR: ${CACHE_DIR}"
+
+echo -e "\n >>> python3 run_dataset.py --task ${TASK}"
 python3 run_dataset.py \
-  --task 1 \
+  --task "${TASK}" \
   --cache_dir "${CACHE_DIR}" \
   --project_root_dir "${ROOT_DIR}" \
   --seed ${SEED} \
+  --bsz "${BSZ}" \
+  --max_seq_len "${MAX_SEQ_LEN}" \
   --hf_id_text_llm "${TEXT_LLM}" \
   --hf_id_code_llm "${CODE_LLM}" \
   --hf_id_vlm "${VLM}" \
-  --bsz "${BSZ}" \
-  --max_seq_len "${MAX_SEQ_LEN}" \
   --verbose
 
 # --interactive
